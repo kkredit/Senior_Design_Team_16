597c82698c3d7e6a434bdd211a4e04ab9c4e3062
Kevin Kredit <Kevin Kredit>
2016-01-26 10:56:36 -0500

    Updated files to reflect actual GardeNet design

--- RadioWork/Node/node/node.ino
+++ RadioWork/Node/node/node.ino
@@ -1,111 +1,65 @@
-
-/** RF24Mesh_Example.ino by TMRh20
- *
- * This example sketch shows how to manually configure a node via RF24Mesh, and send data to the
- * master node.
- * The nodes will refresh their network address as soon as a single write fails. This allows the
- * nodes to change position in relation to each other and the master node.
+/**
+ * node.ino
+ * 
+ * This sketch is for nodes in the GardeNet system. Functions include:
+ *    - Establishing and maintaining a mesh network
+ *    - Controlling a valve
+ *    - Controlling a flow meter (optionally)
+ *    - Relaying information to the master
  */
 
 
+//////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////  Preprocessor   //////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////
+
+// includes
 #include "RF24.h"
 #include "RF24Network.h"
 #include "RF24Mesh.h"
 #include <SPI.h>
 #include <EEPROM.h>
-//#include <printf.h>
 #include <TimerOne.h>
 
+// pins
 #define CE 7
 #define CS 8
+#define FLOWSENSORPIN 2
+#define BUTTON  3
+#define LEDY    6
+#define LEDR    5
 
-/**** Configure the nrf24l01 CE and CS pins ****/
+// mesh settings
+#define nodeID  02
+
+
+//////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////     Globals     //////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////
+
+// mesh network
 RF24 radio(CE, CS);
 RF24Network network(radio);
 RF24Mesh mesh(radio, network);
 
-/**
- * User Configuration: nodeID - A unique identifier for each radio. Allows addressing
- * to change dynamically with physical changes to the mesh.
- *
- * In this example, configuration takes place below, prior to uploading the sketch to the device
- * A unique value from 1-255 must be configuLEDR for each node.
- * This will be stoLEDR in EEPROM on AVR devices, so remains persistent between further uploads, loss of power, etc.
- *
- **/
-#define FLOWSENSORPIN  2
-#define nodeID      02
-#define BUTTON      3
-#define LEDY        6
-#define LEDR        5
+// flow sensor
+volatile uint16_t pulses = 0; // count how many pulses
+volatile uint8_t lastflowpinstate; // track the state of the pulse pin
+volatile uint32_t lastflowratetimer = 0; // you can try to keep time of how long it is between pulses
+volatile float flowrate; // and use that to calculate a flow rate
+bool changedPulseFlag = false; // interrupt is called once a millisecond, looks for any pulses from the sensor
 
+// structs
+
+
+// other
 uint32_t displayTimer = 0;
-int printcount = 0;
 volatile bool hadButtonPress = false;
-bool LEDYstate = false;
+//bool LEDYstate = false;
+int valveState = false;
 bool LEDRstate = false;
+int myStatus = 1;
 
-/*********** Flow sensor vars **************/
-// count how many pulses!
-volatile uint16_t pulses = 0;
-// track the state of the pulse pin
-volatile uint8_t lastflowpinstate;
-// you can try to keep time of how long it is between pulses
-volatile uint32_t lastflowratetimer = 0;
-// and use that to calculate a flow rate
-volatile float flowrate;
-// Interrupt is called once a millisecond, looks for any pulses from the sensor!
-bool changedPulseFlag = false;
-/*******************************************/
-
-struct payload_t {
-  unsigned long ms;
-  unsigned long counter;
-};
-
-//////////////////////////////////////////////////////////////////////////////////
-///////////////////////////////     Setup       //////////////////////////////////
-//////////////////////////////////////////////////////////////////////////////////
-void setup() {
-  pinMode(BUTTON, INPUT_PULLUP);
-  pinMode(LEDR, OUTPUT);
-  pinMode(LEDY, OUTPUT);
-  attachInterrupt(digitalPinToInterrupt(BUTTON), handleButton, FALLING); 
-  //delay(5000);
-  digitalWrite(LEDR, LOW);
-  digitalWrite(LEDY, LOW);
-
-  digitalWrite(LEDR, HIGH);
-  delay(500);
-  digitalWrite(LEDR, LOW);
-  digitalWrite(LEDY, HIGH);
-
-  Serial.begin(9600);
-  // Set the nodeID manually
-  mesh.setNodeID(nodeID);
-  // Connect to the mesh
-  Serial.println("Connecting to the mesh...");
-  Serial.print("Output of mesh.begin(): ");
-  Serial.println(mesh.begin(12, RF24_250KBPS, 2000));
-  //mesh.setAddress(nodeID, nodeID+1); 
-
-  digitalWrite(LEDY, LOW);
-  digitalWrite(LEDR, HIGH);
-  delay(250);
-  digitalWrite(LEDR, LOW);
-  delay(250);
-  digitalWrite(LEDR, HIGH);
-  delay(250);
-  digitalWrite(LEDR, LOW);
-  
-  Timer1.initialize(5000000);
-  Timer1.attachInterrupt(printStatus);
-
-  pinMode(FLOWSENSORPIN, INPUT);
-  digitalWrite(FLOWSENSORPIN, HIGH);
-  lastflowpinstate = digitalRead(FLOWSENSORPIN);
-  useInterrupt(true);
-}
 
 //////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////     ISRs        //////////////////////////////////
@@ -118,19 +72,18 @@ void handleButton(){
     Serial.println(F("Detected buttonpress"));
 }
 
+
 //////////////////////////////////////////////////////////////////////////////////
 /////////////////////////////// Helper Functions//////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////////
 void printStatus(){
-  Serial.println("\n");
-  Serial.println(printcount++);
   Serial.print("\nMy ID:      ");
   Serial.println(mesh.getNodeID());
   Serial.print("My address: ");
   Serial.println(mesh.getAddress(0));
 }
 
-SIGNAL(TIMER0_COMPA_vect) {
+SIGNAL(TIMER0_COMPA_vect){
   uint8_t x = digitalRead(FLOWSENSORPIN);
   
   if (x == lastflowpinstate) {
@@ -143,6 +96,7 @@ SIGNAL(TIMER0_COMPA_vect) {
     pulses++;
     changedPulseFlag = true;
   }
+  
   lastflowpinstate = x;
   flowrate = 1000.0 / lastflowratetimer;  // in hertz
   lastflowratetimer = 0;
@@ -160,55 +114,117 @@ void useInterrupt(boolean v) {
   }
 }
 
+bool safeMeshWrite(void* payload, char header, unsigned datasize){
+  if (!mesh.write(payload, header, datasize)) {
+    // If a write fails, check connectivity to the mesh network
+    if (!mesh.checkConnection() ) {
+      //refresh the network address
+      Serial.println(F("Renewing Address"));
+      mesh.renewAddress();
+    } 
+    else
+      Serial.println(F("Send fail, Test OK"));
+  }
+  else {
+    Serial.print(F("Send OK: "));
+//    Serial.println(*payload);
+  }
+}
+
+int setValve(bool setTo){
+  // set valve
+  valveState = setTo ? 1 : 0;
+
+  // LEDY replicates valve state
+  digitalWrite(LEDY, setTo);
+  return valveState;
+}
+
+
+//////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////     Setup       //////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////
+void setup(){
+  // init LEDs and Button
+  pinMode(BUTTON, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(BUTTON), handleButton, FALLING);
+  pinMode(LEDY, OUTPUT);
+  pinMode(LEDR, OUTPUT);
+
+  // "power-on" light sequence
+  digitalWrite(LEDY, LOW);
+  digitalWrite(LEDR, HIGH);
+  delay(500);
+  digitalWrite(LEDR, LOW);
+  digitalWrite(LEDY, HIGH);
+
+  // begin serial communication
+  Serial.begin(9600);
+
+  // begin mesh communication
+  mesh.setNodeID(nodeID); // do manually
+  Serial.println(F("Connecting to the mesh...\nOutput of mesh.begin(): "));
+  Serial.println(mesh.begin(12, RF24_250KBPS, 2000));
+
+  // "connected" light sequence
+  digitalWrite(LEDY, LOW);
+  digitalWrite(LEDR, HIGH);
+  delay(250);
+  digitalWrite(LEDR, LOW);
+  delay(250);
+  digitalWrite(LEDR, HIGH);
+  delay(250);
+  digitalWrite(LEDR, LOW);
+
+  // init timer for serial communication printing
+  Timer1.initialize(5000000);
+  Timer1.attachInterrupt(printStatus);
+
+  // init flow sensor
+  pinMode(FLOWSENSORPIN, INPUT);
+  digitalWrite(FLOWSENSORPIN, HIGH);
+  lastflowpinstate = digitalRead(FLOWSENSORPIN);
+  useInterrupt(true);
+
+  // close valve
+  setValve(false);
+}
+
+
 //////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////     Loop        //////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////////
 void loop() {
 
+  // refresh the mesh
   mesh.update();
 
-  // handle button presses
+  // handle button presses--send 'B' type message
+  // NOT FINAL CODE
   if(hadButtonPress){
-    // Send an 'B' type message to signal the master that there was a button press
     // 'B' message sends the accumulated water flow, in liters
     float liters = pulses/7.5/60.0;
-    if (!mesh.write(&liters, 'B', sizeof(float))) {
-
+    safeMeshWrite(&liters, 'B', sizeof(float));
+    /*if (!mesh.write(&liters, 'B', sizeof(float))) {
       // If a write fails, check connectivity to the mesh network
-      if ( ! mesh.checkConnection() ) {
+      if (!mesh.checkConnection() ) {
         //refresh the network address
         Serial.println("Renewing Address");
         mesh.renewAddress();
-      } else {
+      } 
+      else
         Serial.println("Send fail, Test OK");
-      }
-    } else {
-      Serial.print("Send OK: "); Serial.println(displayTimer);
     }
+    else {
+      Serial.print("Send OK: ");
+      Serial.println(liters);
+    }*/
 
+    // reset flag
     hadButtonPress = false;
   }
 
-  // Send to the master node every second
-  if (millis() - displayTimer >= 1000) {
-    displayTimer = millis();
-
-    // Send an 'M' type message containing the current millis()
-    if (!mesh.write(&displayTimer, 'M', sizeof(displayTimer))) {
-
-      // If a write fails, check connectivity to the mesh network
-      if ( ! mesh.checkConnection() ) {
-        //refresh the network address
-        Serial.println(F("Renewing Address"));
-        mesh.renewAddress();
-      } else {
-        Serial.println(F("Send fail, Test OK"));
-      }
-    } else {
-      Serial.print("Send OK: "); Serial.println(displayTimer);
-    }
-  }
-
+  // handle flow meter pulses
   if (changedPulseFlag == true){
     Serial.print("Freq: "); Serial.println(flowrate);
     Serial.print("Pulses: "); Serial.println(pulses, DEC);
@@ -224,15 +240,46 @@ void loop() {
     Serial.print(liters); Serial.println(" Liters");
   }
 
-  while (network.available()) {
+  // read in messages
+  while (network.available()){
     RF24NetworkHeader header;
-    payload_t payload;
-    network.read(header, &payload, sizeof(payload));
-    Serial.print("Received packet #");
-    Serial.print(payload.counter);
-    Serial.print(" at ");
-    Serial.println(payload.ms);
+    void* payload = malloc(sizeof(float)); // float is largest possible size
+    network.read(header, payload, sizeof(payload));
+    Serial.print(F("Received ")); Serial.print(header.type); Serial.println(F(" type message."));
+
+    switch(header.type){
+    case 'V':
+      // Valve command, on or off; type is bool
+      //  set the value and send return message      
+      setValve( *((bool*)payload) );
+      safeMeshWrite(&valveState, 'v', sizeof(valveState));
+      break;
+    case 'R':
+      // Request; type is char
+      switch( *((char*)payload) ){
+      case 'v':
+        // tell current valve state
+        safeMeshWrite(&valveState, 'v', sizeof(valveState));
+        break;
+      case 'r':
+        // tell current flow rate
+        float liters = pulses/7.5/60.0; // TODO: make flowrate; currently is accumulated water
+        safeMeshWrite(&liters, 'r', sizeof(liters));
+        break;
+      case 'n':
+        // return status
+        safeMeshWrite(&myStatus, 'r', sizeof(myStatus));
+        break;
+      default:
+        break;
+      }
+      
+      break;
+    default:
+      break;
+    }
+
+    free(payload);
   }
 }
-
 
