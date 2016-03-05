3826e7875f5bf8a2ed70d6e66397b71b4177f104
cak33 <kingstoncharles5@gmail.com>
2016-03-05 23:34:48 +0000

    Changed the port number

--- garden_net/gn_util/controller_interface.py
+++ garden_net/gn_util/controller_interface.py
@@ -17,7 +17,7 @@ SOCKET_LIST = []
 soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 #soc.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 host = socket.gethostname()
-port = 5522
+port = 5530
 print("The server hostname is: " + host)
 
 soc.bind(('', port))
