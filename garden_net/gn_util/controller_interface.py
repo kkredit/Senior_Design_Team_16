5fe1e757fa68e1f5b3a86633da324bf6b5e640ed
Charles Kingston <kingstoncharles5@gmail.com>
2016-03-10 10:46:33 -0500

    port now 5532

--- garden_net/gn_util/controller_interface.py
+++ garden_net/gn_util/controller_interface.py
@@ -18,7 +18,7 @@ SOCKET_LIST = []
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 #soc.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 host = socket.gethostname()
-port = 5531
+port = 5532
 print("The server hostname is: " + host + " on port: " + str(port))
 
 server_socket.bind(('', port))
